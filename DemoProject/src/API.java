537c781ed64fa8c0063c1ab5c8931f03ddf15ec4
Hong <Hong@DESKTOP-MFU0MJG>
2020-06-12 16:14:38 +0800

    This is from API branch

---
+++ DemoProject/src/API.java
@@ -0,0 +1,6 @@
+
+public class API {//this is new
+	
+	int i=0;//this is new
+
+}
--- DemoProject/src/Main.java
+++ DemoProject/src/Main.java
@@ -6,9 +6,12 @@ public class Main {
 		System.out.println("Hello World");
 		System.out.println("Hello you");
 		System.out.println("Hello noob");
+		System.out.println("Hello everybody"); //this is new
 		System.out.println("Hello us");
 		
 		System.out.println("Add new feature A");
+		
+		System.out.println("Connect to API");//this is new
 		
 	}
 	
